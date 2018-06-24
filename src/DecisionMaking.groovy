88233862a269ebfabc20ff9d1105ce8844a8f74b
Victor Mwenda <vmwenda.vm@gmail.com>
2018-06-24 13:21:45 +0300

    DecisionMaking : Switch statements

--- src/DecisionMaking.groovy
+++ src/DecisionMaking.groovy
@@ -2,15 +2,18 @@ class DecisionMaking {
 
     static void main(String... args) {
 
-        def index = 13
+        def items = 0..5
 
-        if (index > 10) {
-            println "index greater than 10"
-        } else if (index < 10) {
-            println "index less than 10"
-        } else {
-            println "index cannot be quantified"
-        }
+        for (int item = 0; item < items.size(); item++)
+            switch (items.get(item)) {
+                case 0: println "Go to origin"
+                    break
+                case 1: println "Release first Release Candidate"
+                    break
+                case 2: println "Operate from first Disaster Recovery site"
+                    break
+                default: println "Deploy to Disaster Recovery site $item"
+            }
 
     }
 
