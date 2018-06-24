32fb0d566ae8666318a2e7c38a5c88f594322ecf
Victor Mwenda <vmwenda.vm@gmail.com>
2018-06-24 13:03:41 +0300

    Loops: for loops

--- src/Loops.groovy
+++ src/Loops.groovy
@@ -1,3 +1,19 @@
-
 class Loops {
+
+    static void main (String ... args) {
+
+        //while loops
+        def count = 0
+        while(count < 10){
+            println "Count now is at $count";
+            ++count
+        }
+
+        //for loops
+        for(int i = 0; i < 10; i++){
+            println "i now is at $i";
+        }
+
+    }
+
 }
