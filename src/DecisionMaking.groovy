a6f4eabe6c6d9c787e99bb393aaaeca56d9c5b50
Victor Mwenda <vmwenda.vm@gmail.com>
2018-06-24 13:16:23 +0300

    DecisionMaking : Using if, else , nested if else

---
+++ src/DecisionMaking.groovy
@@ -0,0 +1,17 @@
+class DecisionMaking {
+
+    static void main(String... args) {
+
+        def index = 13
+
+        if (index > 10) {
+            println "index greater than 10"
+        } else if (index < 10) {
+            println "index less than 10"
+        } else {
+            println "index cannot be quantified"
+        }
+
+    }
+
+}
