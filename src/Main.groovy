4c34bcf3d6c89d050fd263f32e5fcd17201de61f
Victor Mwenda <vmwenda.vm@gmail.com>
2018-06-24 12:45:15 +0300

    Variables : Defining and working with variables

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,5 +1,16 @@
 class Main {
     static void main(String [] args) {
-        println ("Hello world");
+
+        def name = "Victor Mwenda";
+
+        println( "His name is ${name}")
+        println( "His name is $name")
+
+        def yob = 1993;
+        def curYr = 2018;
+
+        println ("His age is $curYr - $yob")
+        println ("His age is ${curYr} - ${yob}")
+
     }
 }
